0e44063cbc90388802292c6f34ed999235e4981c
Hien Luong Tran Hy <hyhien@gmail.com>
2025-06-27 07:53:40 +0700

    Demo Upload single file

---
+++ Buoi05/Lab03/Controllers/FileUploadController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UploadFile(IFormFile MyFile)
+        {
+            if (MyFile == null)
+            {
+                TempData["Message"] = "Chưa có file upload";
+            }
+            else
+            {
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", MyFile.FileName);
+                using(var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    MyFile.CopyTo(file);
+                }
+                TempData["Message"] = "Upload file thành công";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
