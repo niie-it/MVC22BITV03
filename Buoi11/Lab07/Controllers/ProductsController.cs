58ed5829dfe17f8db3570ddeaacd1ea03c4b8920
Hien Luong Tran Hy <hyhien@gmail.com>
2025-07-19 09:50:50 +0700

    Init day12

---
+++ Buoi11/Lab07/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Lab07.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab07.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly MvcNiieLabContext _context;
+
+		public ProductsController(MvcNiieLabContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var data = _context.Products
+				.Include(p => p.Supplier)
+				.Include(p => p.Category);
+			return View(data.ToList());
+		}
+	}
+}
