39628cb163396ba2627891184e14f133c38bf02e
Hien Luong Tran Hy <hyhien@gmail.com>
2025-08-02 11:14:39 +0700

    Demo Authen = Individual acoount

---
+++ MyEShop01/MyEShop01/Controllers/AjaxController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyEShop01.Entities;
+using MyEShop01.Models;
+
+namespace MyEShop01.Controllers
+{
+	public class AjaxController : Controller
+	{
+		private readonly MyEshopContext _context;
+
+		public AjaxController(MyEshopContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult Search()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Search(string keyword)
+		{
+			var dsHangHoa = _context.HangHoas.Where(p => p.TenHh.Contains(keyword));
+
+			var data = dsHangHoa.Select(hh => new KetQuaTimKiemVM
+			{
+				MaHh = hh.Id,
+				TenHh = hh.TenHh,
+				Hinh = hh.Hinh,
+				DonGia = hh.DonGia.Value,
+				NgaySX = hh.NgaySx,
+				Loai = hh.MaLoaiNavigation.TenLoai
+			}).ToList();
+			return PartialView("TimKiemPartial", data);
+		}
+
+
+
+		public IActionResult TimKiem()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult TimKiem(string? name, double? fromPrice, double? toPrice)
+		{
+			var dsHangHoa = _context.HangHoas.Include(hh => hh.MaLoaiNavigation).AsQueryable();
+			if (name != null)
+			{
+				dsHangHoa = dsHangHoa.Where(hh => hh.TenHh.Contains(name));
+			}
+			if (fromPrice != null)
+			{
+				dsHangHoa = dsHangHoa.Where(hh => hh.DonGia.Value >= fromPrice);
+			}
+			if (toPrice != null)
+			{
+				dsHangHoa = dsHangHoa.Where(hh => hh.DonGia.Value <= toPrice);
+			}
+			var data = dsHangHoa.Select(hh => new KetQuaTimKiemVM
+			{
+				MaHh = hh.Id,
+				TenHh = hh.TenHh,
+				Hinh = hh.Hinh,
+				DonGia = hh.DonGia.Value,
+				NgaySX = hh.NgaySx,
+				Loai = hh.MaLoaiNavigation.TenLoai
+			}).ToList();
+			return Json(data);
+		}
+
+	}
+}
---
+++ MyEShop01/MyEShop01/Models/KetQuaTimKiemVM.cs
@@ -0,0 +1,16 @@
+﻿namespace MyEShop01.Models
+{
+	public class KetQuaTimKiemVM
+	{
+		public int MaHh { get; set; }
+		public string TenHh { get; set; }
+		public string Hinh { get; set; }
+		public double DonGia { get; set; }
+		public DateTime NgaySX { get; set; }
+		public string Loai
+		{
+			get; set;
+
+		}
+	}
+}
